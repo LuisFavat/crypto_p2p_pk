c34776c8091769faca01b2b348d9927b27de871f
LuisFavat <73365264+LuisFavat@users.noreply.github.com>
2023-08-25 19:30:01 -0300

    Proyecto inicial

---
+++ src/main/java/ar/edu/unq/cryptop2p/CryptoP2pApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.cryptop2p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoP2pApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoP2pApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unq/cryptop2p/CryptoP2pApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.cryptop2p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoP2pApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
