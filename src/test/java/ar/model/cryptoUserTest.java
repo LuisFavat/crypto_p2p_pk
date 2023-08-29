9364f8419b739201f52235e4a860c1c226f0cfcc
LuisFavat <73365264+LuisFavat@users.noreply.github.com>
2023-08-28 23:26:11 -0300

    update test

--- src/test/java/ar/model/cryptoUserTest.java
+++ src/test/java/ar/model/cryptoUserTest.java
@@ -1,9 +1,10 @@
 package ar.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Test;
 
+@SpringBootTest
 public class cryptoUserTest {
     @Test
     void lala(){
