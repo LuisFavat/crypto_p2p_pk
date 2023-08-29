9f0c31fd489691c0ac4cbc68843630f2aab1b3dc
LuisFavat <73365264+LuisFavat@users.noreply.github.com>
2023-08-28 23:05:58 -0300

    testing actions

---
+++ src/test/java/ar/model/cryptoUserTest.java
@@ -0,0 +1,12 @@
+package ar.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class cryptoUserTest {
+    @Test
+    void lala(){
+        assertEquals(0, 100);
+    }
+}
